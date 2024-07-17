03f3f0121d340584c08b55c85e76db50ba69d6b6
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2024-07-17 18:34:21 +0530

    Update Frog Jump.java

--- DP/Frog Jump.java
+++ DP/Frog Jump.java
@@ -45,3 +45,23 @@ public class Solution {
         return dp[n-1];
     }
 }
+
+
+// Space Optimization
+public class Solution {
+    public static int frogJump(int n, int heights[]) {
+        int prev=0;
+        int prev2=0;
+        for(int i=1;i<n;i++){
+            int one=prev + Math.abs(heights[i]-heights[i-1]);
+            int two=Integer.MAX_VALUE;
+            if(i>1){
+                two=prev2 + Math.abs(heights[i]-heights[i-2]);
+            }
+            int curr=Math.min(one,two);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+    }
+}
