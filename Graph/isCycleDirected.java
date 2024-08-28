b644e0f63738cf818e1ea6d817a03293b03c7964
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2024-08-28 22:45:04 +0530

    Create isCycleDirected.java

---
+++ Graph/isCycleDirected.java
@@ -0,0 +1,29 @@
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        
+        int[] vis=new int[V];
+        int[] PathVis=new int[V];
+        for(int i=0;i<V;i++){
+            if(vis[i]==0){
+                if(solve(i,V,vis,PathVis,adj)) return true;
+            }
+        }
+        return false;
+    }
+    
+    boolean solve(int node,int V,int[] vis,int[] PathVis,ArrayList<ArrayList<Integer>> adj){
+        vis[node]=1;
+        PathVis[node]=1;
+        for(int i:adj.get(node)){
+            if(vis[i]==0){
+                if(solve(i,V,vis,PathVis,adj))
+                    return true;
+            }
+            else if(PathVis[i]==1) return true;
+        }
+        
+        PathVis[node]=0;
+        return false;
+    }
+}
