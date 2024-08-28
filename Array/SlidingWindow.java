099a763ee37e5c71c77299d14230f7b8672c7a4a
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2024-08-28 11:48:39 +0530

    Create SlidingWindow.java

---
+++ Array/SlidingWindow.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static ArrayList<Integer> subarraySum(int[] arr, int n, int s) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        int sum=0,j=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            while(sum>s && j<i){    // Here i do always mistake Remember!!!!
+                sum-=arr[j];
+                j++;
+            }
+            if(sum==s){
+                ans.add(j+1);
+                ans.add(i+1);
+                return ans;
+            }
+        }
+        ans.add(-1);
+        return ans;
+    }
+} 
