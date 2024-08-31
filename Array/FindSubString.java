33d8966a68a4eddba453f89f8c3ee8e1ac84b089
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2024-09-01 00:35:07 +0530

    Create FindSubString.java

---
+++ Array/FindSubString.java
@@ -0,0 +1,20 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String sent = "hello world ok,! bye";
+        String search = "ok";
+        String[] arr = sent.split(" ");
+        boolean found = false; 
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].replaceAll("[^a-zA-Z]", "");
+            if(arr[i].equals(search)) {
+                System.out.println("Found");
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Not Found");
+        }
+    }
+}
