fcf9f53660c05f2cc34c93008be69053caf31e8e
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2025-02-22 16:20:27 +0530

    Create isBalanced.java

---
+++ Tree/isBalanced.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return solve(root)==-1?false:true;
+    }
+    int solve(TreeNode root){
+        if(root==null) return 0;
+        int left=solve(root.left);
+        if(left==-1) return -1;
+        int right=solve(root.right);
+        if(right==-1) return -1;
+        
+        if(Math.abs(left-right)>1) return -1;
+        return Math.max(left,right)+1;
+    }
+}
