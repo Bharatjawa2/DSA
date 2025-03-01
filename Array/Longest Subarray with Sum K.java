0883be8a7a24e8d3853e515c8c600cb789fa2438
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2025-03-01 13:36:58 +0530

    Update Longest Subarray with Sum K.java

--- Array/Longest Subarray with Sum K.java
+++ Array/Longest Subarray with Sum K.java
@@ -1,6 +1,6 @@
 class Solution {
     public int longestSubarray(int[] arr, int k) {
-        HashMap<Integer,Integer> map=new HashMap<>();
+        HashMap<Integer,Integer> map=new HashMap<>(); // prefixSumMap
         int sum=0,max=0;
         
         for(int i=0;i<arr.length;i++){
@@ -12,7 +12,7 @@ class Solution {
                 max=Math.max(max,i-map.get(sum-k));
             }
             if(!map.containsKey(sum)){
-                map.put(sum,i);
+                map.put(sum,i);    // bcs we have to find longest so not to updat newest.
             }
         }
         return max;
