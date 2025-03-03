23e0a44f1b6edb885785cfed93aaf1c88229ca50
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2025-03-03 12:36:45 +0530

    Create Longest Consecutive Integer.java

---
+++ Array/Longest Consecutive Integer.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n=nums.length;
+        HashSet<Integer> set=new HashSet<>();
+        for(int i:nums) set.add(i);
+
+        int max=0;
+        for(int num:set){
+            if(!set.contains(num-1)){
+                int length=1;
+                while(set.contains(num+length)){
+                    length++;
+                }
+                max=Math.max(max,length);
+            }
+        }
+        return max;
+    }
+    
+}
