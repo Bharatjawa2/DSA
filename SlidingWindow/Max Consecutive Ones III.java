3f195a0286da0391505b79964053fa9b084dbdfd
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2025-03-27 15:01:47 +0530

    Update Max Consecutive Ones III.java

--- SlidingWindow/Max Consecutive Ones III.java
+++ SlidingWindow/Max Consecutive Ones III.java
@@ -1,48 +1,17 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int n=nums.length;
+        int left=0;
+        int max=0;
         int count=0;
-        int left=0,right=0,max=0;
-        
-        while(right<n){
-            if(nums[right]==0){
-                count++;
-            }
 
+        for(int right=0;right<n;right++){
+            if(nums[right]==0) count++;
             while(count>k){
                 if(nums[left]==0) count--;
                 left++;
             }
-
             max=Math.max(max,right-left+1);
-            right++;
-        }
-        return max;
-    }
-}
-
-// Remove While loop (Dont go below MAX Length)
-
-class Solution {
-    public int longestOnes(int[] nums, int k) {
-        int n=nums.length;
-        int count=0;
-        int left=0,right=0,max=0;
-        
-        while(right<n){
-            if(nums[right]==0){
-                count++;
-            }
-
-            if(count>k){
-                if(nums[left]==0) count--;
-                left++;
-            }
-
-            if(count<=k){
-                max=Math.max(max,right-left+1);
-            }
-            right++;
         }
         return max;
     }
