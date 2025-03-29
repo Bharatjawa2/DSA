f0bfeb2182229d08f3c3789822bfdb313df45dad
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2025-03-29 18:46:08 +0530

    Update Maximum Points You Can Obtain from Cards.java

--- SlidingWindow/Maximum Points You Can Obtain from Cards.java
+++ SlidingWindow/Maximum Points You Can Obtain from Cards.java
@@ -1,14 +1,14 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
         int n=cardPoints.length;
-        int lsum=0;
-        for(int i=0;i<k;i++) lsum+=cardPoints[i];
-        int max=lsum;
+        int left=0;
+        for(int i=0;i<k;i++) left+=cardPoints[i];
+        int max=left;
 
         for(int i=k-1;i>=0;i--){
-            lsum-=cardPoints[i];
-            lsum+=cardPoints[--n];
-            max=Math.max(max,lsum);
+            left-=cardPoints[i];
+            left+=cardPoints[--n];
+            max=Math.max(max,left);
         }
         return max;
     }
