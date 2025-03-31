ed70e4a41d8fbecb4d0d85ae0e16a6afb1f4dd0a
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2025-03-31 18:20:28 +0530

    Create Kth Smallest Element.java

---
+++ Heap/Kth Smallest Element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int kthSmallest(int[] arr, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i:arr){
+            pq.add(i);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
