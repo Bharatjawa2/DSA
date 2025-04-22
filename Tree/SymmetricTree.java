116956d057d05cb53a5f407d719dd48921461a36
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2025-04-22 23:04:37 +0530

    Create SymmetricTree.java

---
+++ Tree/SymmetricTree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        return solve(root.left,root.right);
+    }
+    boolean solve(TreeNode root1,TreeNode root2){
+        if(root1==null && root2==null) return true;
+        if(root1==null || root2==null) return false;
+        if(root1.val!=root2.val) return false; 
+        return solve(root1.left,root2.right) && solve(root1.right,root2.left);
+    }
+}
