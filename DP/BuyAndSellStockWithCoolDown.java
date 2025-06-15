ced3d652ea2d2b3d6f913279d80afd80645b9ffb
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2025-06-15 12:55:02 +0530

    Create BuyAndSellStockWithCoolDown.java

---
+++ DP/BuyAndSellStockWithCoolDown.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        Integer[][] dp=new Integer[n][2];
+        return solve(0,1,n,prices,dp);
+    }
+    int solve(int index,int buy,int n,int[] prices,Integer[][] dp){
+        if(index>=n) return 0;
+        if(dp[index][buy]!=null) return dp[index][buy];
+        int not=solve(index+1,buy,n,prices,dp);
+        int take=0,sell=0;
+        if(buy==1){
+            take=solve(index+1,0,n,prices,dp)-prices[index];
+        }
+        else{
+            sell=solve(index+2,1,n,prices,dp)+prices[index];
+        }
+        return dp[index][buy]=Math.max(take,Math.max(sell,not));
+    }
+}
