167f6bae26625670841c577a61c8c1a95be86b0b
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2025-06-16 17:11:42 +0530

    Update MatrixChainMultiplication.java

--- DP/MatrixChainMultiplication.java
+++ DP/MatrixChainMultiplication.java
@@ -1,8 +1,22 @@
 class Solution {
     static int matrixMultiplication(int arr[]) {
         int n=arr.length;
-        Integer[][] dp=new Integer[n][n];
-        return solve(1,n-1,arr,dp);
+        // Integer[][] dp=new Integer[n][n];
+        // return solve(1,n-1,arr,dp);
+        int[][] dp=new int[n][n];
+        for(int i=0;i<n;i++) dp[i][i]=0;
+        
+        for(int i=n-1;i>=1;i--){
+            for(int j=i+1;j<n;j++){
+                int min=Integer.MAX_VALUE;
+                for(int k=i;k<j;k++){
+                    int step=arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+                    min=Math.min(min,step);
+                }
+                dp[i][j]=min;
+            }
+        }
+        return dp[1][n-1];
     }
     static int solve(int i,int j,int[] arr,Integer[][] dp){
         if(i==j) return 0;
